22501398957bf4816c3062125c439829216aa4f8
mazah <mazah@users.noreply.github.com>
2016-07-16 17:47:10 +0300

    more fixes

--- src/org/traccar/BaseProtocolDecoder.java
+++ src/org/traccar/BaseProtocolDecoder.java
@@ -87,6 +87,7 @@ public abstract class BaseProtocolDecoder extends ExtendedObjectDecoder {
             position.setAltitude(last.getAltitude());
             position.setSpeed(last.getSpeed());
             position.setCourse(last.getCourse());
+            position.setAddress(last.getAddress());
         } else {
             position.setFixTime(new Date(0));
         }
