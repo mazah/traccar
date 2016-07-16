44503e1ffd95313669f3eb31d6d0bd7dc4d6d816
mazah <mazah@users.noreply.github.com>
2016-07-16 16:49:56 +0300

    Trying to fix command response

--- src/org/traccar/protocol/Gt06ProtocolDecoder.java
+++ src/org/traccar/protocol/Gt06ProtocolDecoder.java
@@ -258,7 +258,7 @@ public class Gt06ProtocolDecoder extends BaseProtocolDecoder {
 
                     if (commandLength > 0) {
                         buf.readUnsignedByte(); // server flag (reserved)
-                        position.set("command", buf.readBytes(commandLength - 1).toString(StandardCharsets.US_ASCII));
+                        position.set(Position.KEY_RESULT, buf.readBytes(commandLength - 1).toString(StandardCharsets.US_ASCII));
                     }
 
                     buf.readUnsignedShort(); // language
